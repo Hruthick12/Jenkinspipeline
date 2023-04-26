b1314782f780a600add7c57b7566927648db72f6
hruthik <hruthik@gmail.com>
2023-04-26 10:56:37 +0530

    need to be commited

---
+++ vars/checkout_git.groovy
@@ -0,0 +1,6 @@
+def checkout_git(){
+    echo "this is for checking out git code."
+            git credentialsId: 'github', url:'git@github.com:Hruthick12/java-hello-world-with-maven.git'
+
+}
+return this
