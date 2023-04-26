9f4425d1446ebbf2a598562178cf49cf1845a8ac
hruthik <hruthik@gmail.com>
2023-04-26 11:46:16 +0530

    changes

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,6 +1,7 @@
-def checkout_git(){
+def checkout_git(string repo){
+
     echo "this is for checking out git code."
-            git credentialsId: 'github', url:'git@github.com:Hruthick12/java-hello-world-with-maven.git'
+            git credentialsId: 'github', url:'git@github.com:Hruthick12/${repo}.git'
 
 }
 return this
